28553afdfb8a3e26700bc3ffc1cec4740c35a599
OwenBrush <owenbrush@gmail.com>
2022-07-18 17:08:14 -0400

    refactored helpers to make all functions more consistent

--- helpers.py
+++ helpers.py
@@ -1,8 +1,5 @@
-SAVE_DIR = 'data/'
-SERVER = 'https://divvy-tripdata.s3.amazonaws.com/'
-FILE_NAME = '-divvy-tripdata'
-DELAY = 5
 
+from xmlrpc.client import DateTime
 from zipfile import ZipFile
 import os
 import requests
@@ -11,32 +8,40 @@ import time
 from IPython.display import clear_output
 import glob
 import pandas as pd
+from datetime import datetime
+from meteostat import Point, Daily
 
+START_DATE = datetime(2020, 6, 1)
+END_DATE = datetime(2022, 5, 31)
+CHICAGO = Point(41.881832, -87.62317, 182)
+BIKE_SAVE = 'data/bike/'
+COVID_SAVE = 'data/covid/covid_data.csv'
+WEATHER_SAVE = 'data/weather/weather_data.csv'
+SERVER = 'https://divvy-tripdata.s3.amazonaws.com/'
+FILE_NAME = '-divvy-tripdata'
+DELAY = 5
 
-def download_data(save_dir:str=SAVE_DIR, start_date:int=202006, n_months=24, retries = 3) -> None:
+def download_bike_data(start_date:DateTime=START_DATE, end_date:DateTime=END_DATE) -> None:
     """
     Checks save directory for apropriate csv files and if they are not present, downloads them.
     """ 
-    ### TODO: add Covid Data: https://data.cityofchicago.org/api/views/naz8-j4nc/rows.csv?accessType=DOWNLOAD
-
-    def _downloand_and_extract(file, delay=DELAY):
+    def _downloand_and_extract(file, delay=DELAY) -> None:
         try:
             response = requests.get(SERVER+file+'.zip', stream=True)
             zip = ZipFile(BytesIO(response.content))
-            zip.extractall(path=SAVE_DIR)
+            zip.extractall(path=BIKE_SAVE)
         except requests.exceptions.RequestException as e: 
             raise e
         time.sleep(delay)
 
-    def _download_all(date) -> list:
+    def _download_all() -> list:
         failed_attempts = []
-        for i in range(0,n_months):
-            month = (int(str(date)[-2:]) +i) 
-            year = int(str(date)[:4]) + int((month-1) / 12)
-            iter_date = str(year) + str((month-1) % 12 + 1).zfill(2)
-            file = str(iter_date)+FILE_NAME
-            if not os.path.exists(SAVE_DIR+file+'.csv'):
-                print(f'{i+1}/{n_months} : Downloading {file+".csv"}')
+        dates = pd.date_range(start_date, end_date, freq='1M') 
+        for i, date in enumerate(dates):
+            iter_date = str(date.year)+str(date.month).zfill(2)
+            file = iter_date+FILE_NAME
+            if not os.path.exists(BIKE_SAVE+file+'.csv'):
+                print(f'{i+1}/{len(dates)} : Downloading {file+".csv"}')
                 try:
                     _downloand_and_extract(file)
                 except:
@@ -47,10 +52,10 @@ def download_data(save_dir:str=SAVE_DIR, start_date:int=202006, n_months=24, ret
 
         return failed_attempts
 
-    def _retry_failed_downloads(files:list) -> list:
+    def _retry_failed_downloads(files:list, retries:int=3) -> list:
         for file in files.copy():
             for i in range (0, retries):
-                if not os.path.exists(SAVE_DIR+file+'.csv'):
+                if not os.path.exists(BIKE_SAVE+file+'.csv'):
                     try:
                         print(f'Re-attemping to download {SERVER+file+".zip"}, attempt #{i+1}')
                         _downloand_and_extract(file, delay=DELAY*2)
@@ -59,17 +64,34 @@ def download_data(save_dir:str=SAVE_DIR, start_date:int=202006, n_months=24, ret
                         pass
         return files
             
-    failed_attempts = _download_all(start_date)
+    failed_attempts = _download_all()
     failed_attempts = _retry_failed_downloads(failed_attempts)
 
     clear_output(wait=True)
     if len(failed_attempts) == 0:
-        print('All files downloaded successfully')
+        print('All divvy bike files downloaded successfully')
     else:
         print(f'Unable to download: {failed_attempts}') 
+        
+def download_covid_data():
+    try:
+        data = pd.read_csv('https://data.cityofchicago.org/api/views/naz8-j4nc/rows.csv?accessType=DOWNLOAD')
+        data.to_csv(COVID_SAVE)
+        print('Covid data downloaded succesfully')
+    except requests.exceptions.RequestException as e:
+        print('Unable to download covid data') 
+        raise e
 
+def download_weather_data(start:DateTime=START_DATE, end:DateTime=END_DATE, location:Point=CHICAGO):
+    try:
+        data = Daily(location, start, end)
+        data = data.fetch()
+        data.to_csv(WEATHER_SAVE)
+        print('Weather data downloaded succesfully')
+    except:
+        print('Unable to download weather data')
 
-def assemble_data_frame(save_dir:str=SAVE_DIR) -> pd.DataFrame:
+def assemble_bike_data(save_dir:str=BIKE_SAVE) -> pd.DataFrame:
     """
     Returns DataFrame containing all csv files in given directory"""
     files = glob.glob(save_dir + '/*.csv')
@@ -79,6 +101,5 @@ def assemble_data_frame(save_dir:str=SAVE_DIR) -> pd.DataFrame:
 
 
             
-
 
 
